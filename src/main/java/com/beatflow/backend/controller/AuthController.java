a075e2cc015a13ca6085969fe0e68546f8d70e3b
Karthik <muralikarthik890123@gmail.com>
2025-09-24 14:10:17 +0530

    Update AuthController.java

--- src/main/java/com/beatflow/backend/controller/AuthController.java
+++ src/main/java/com/beatflow/backend/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://34.228.73.21")
+@CrossOrigin(origins = "*")
 public class AuthController {
     
     private final AuthService authService;
