00905cd51fa19ab95b36925b5a440939a36fb53e
Karthik <muralikarthik890123@gmail.com>
2025-09-24 16:24:08 +0530

    Update SecurityConfig.java

--- src/main/java/com/beatflow/backend/config/SecurityConfig.java
+++ src/main/java/com/beatflow/backend/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://3.95.37.32", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://54.234.12.86", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(false);
